c90b9ada98ba682f36cd5d61a07df67cbb758d7a
Conrad Knapp <conrad.knapp@bc.edu>
2018-01-25 01:16:03 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>React with Parcel</div>;
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
