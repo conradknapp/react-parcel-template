0617750757b58969ccc6469084c6b6f97f97be58
Conrad Knapp <conrad.knapp@bc.edu>
2018-01-28 03:18:04 -0500

    Reorders directories a bit (adds public folder and puts index.html in it), changes 'dev' script to 'start', puts style.css file and App.js files in src

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./style.css";
+
+const App = () => {
+  return <div>React with Parcel Starter</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector(".container"));
